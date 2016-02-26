06291efa7f3010135358aae393de9af694c85ab8
gotuzzoj23 <gotuzzoj23@gmail.com>
2016-02-26 13:48:15 -0800

    finished assignment 2

--- solution--gotuzzoj23/buffer.cpp
+++ solution--gotuzzoj23/buffer.cpp
@@ -6,6 +6,10 @@
 //  Copyright © 2016 Jose Gotuzzo. All rights reserved.
 //
 
+/**
+ * Implements `buffer.h`.
+ */
+// TODO: `#include`s for system headers, if necessary
 
 #include<iostream>
 #include<cstdlib>
@@ -14,18 +18,37 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+// TODO: `#include`s for other local headers, if necessary
 
+// ----------------------------------------------------------------------------
 
 Buffer::Buffer(unsigned int size_x, unsigned int size_y) : size_x(size_x), size_y(size_y)
 {
     data_ = new char[size_x*size_y];
     clear();
 }
-
+/**
+ * The constructor.
+ *
+ * Must initialize `size_x` and `size_y`, and allocate memory for (and
+ * initialize) `data_`.
+ *
+ * Notes:
+ * - Since `size_x` and `size_y` are constant, they must be initialized
+ *   in the initialization list, rather than set in the constructor
+ *   body.
+ * - You may assume that the dynamic memory allocation succeeds.
+ */
 
 Buffer::~Buffer() {
     delete[] data_;
 }
+/**
+ * The destructor.
+ *
+ * Must free the memory that the constructor allocated to `data_`.
+ */
+
 
 char Buffer::get(unsigned int x, unsigned int y) const {
     
@@ -36,6 +59,16 @@ char Buffer::get(unsigned int x, unsigned int y) const {
     return data_[y * size_x + x];
 }
 
+/**
+ * Return the character at `data_[ y * size_x + x ]`.
+ *
+ * If `x` or `y` is out of bounds, should write
+ * ```
+ * "ERROR: `Buffer::get`: index out of bounds\n"
+ * ```
+ * to `cerr` and `exit(1)`.
+ */
+
 void Buffer::set(unsigned int x, unsigned int y, char c) {
     
     if (x >= size_x || y >= size_y)
@@ -45,6 +78,15 @@ void Buffer::set(unsigned int x, unsigned int y, char c) {
     }
     data_[y * size_x + x] = c;
 }
+/**
+ * Set the character at `data_[ y * size_x + x ]` to `c`.
+ *
+ * If `x` or `y` is out of bounds, should write
+ * ```
+ * "ERROR: `Buffer::set`: index out of bounds\n"
+ * ```
+ * to `cerr` and `exit(1)`.
+ */
 
 void Buffer::set(unsigned int pos_x, unsigned int pos_y, std::string s)
 {
@@ -62,6 +104,38 @@ void Buffer::set(unsigned int pos_x, unsigned int pos_y, std::string s)
     }
 }
 
+/**
+ * Overlay `s` onto `data_`.
+ *
+ * For example, if this buffer is representing an array that looks like
+ * ```
+ * *******
+ * *******
+ * *******
+ * *******
+ * *******
+ * ```
+ * and we call `set(1, 2, "hello\nworld")`, the buffer's contents
+ * should now be
+ * ```
+ * *******
+ * *******
+ * *hello*
+ * *world*
+ * *******
+ * ```
+ *
+ * Notes:
+ * - This function may call the other `set` (which takes a `char` as
+ *   its last argument), if you like.  In that case, you may leave the
+ *   index error checking to the other `set`.  Otherwise, you should
+ *   have checks in this function to make sure you're not putting
+ *   characters where they don't belong.
+ * - This function will be useful when implementing the `Shape` class.
+ * - If you can't think of a way to do this, see
+ *   `hint--implementation-of-Buffer-set.md`.
+ */
+
 
 void Buffer::draw() const
 {
@@ -73,9 +147,24 @@ void Buffer::draw() const
     cout << endl;
 }
 
+
+
+/*OUTPUT THE CONTENTS OF THE BUFFER TO 'COUT'.
+ *[Feb 17] I have no idea if this is right, if you guys could, correct it,
+ * i may look online, dont know, but yea
+ */
+
+
+
+
 void Buffer::clear()
 {
     for (unsigned int z = 0; z < size_x*size_y; z++)
         data_[z] = ' ';
     
 };
+
+/*seT each element data_ " "*/
+
+/*Robert(Feb 9,16)- So, not sure what to do for the "void Buffer::set(unsigned int pos_x, unsigned int pos_y, std::string s)"
+ *funciton, so i decided to do this one real quick. Go ahead and check if it's right, please*/
